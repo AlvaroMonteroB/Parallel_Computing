a6e31464d1dd65dee41c601c14badb0b6bcf7018
AlvaroMonteroB <alvaromonterob@outlook.com>
2024-09-26 17:31:43 -0600

    p3 ini

---
+++ Practica3/p3_1.c
@@ -0,0 +1,12 @@
+//Montero Barraza Álvaro David
+//Villeda Patricio Arón Alejandro
+//Calderon Sabbagh Juan Alberto
+//Olicon Alvarez Jessica Guadalupe
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<wait.h>
+#include<sys/types.h>
+#include<sys/shm.h>
