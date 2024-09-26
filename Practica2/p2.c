f48a426e2bb1ed01b94d9d6f816d7697fceb0a1e
AlvaroMonteroB <alvaromonterob@outlook.com>
2024-09-26 01:44:54 -0600

    NOmbres

--- Practica2/p2.c
+++ Practica2/p2.c
@@ -1,3 +1,9 @@
+//Montero Barraza Álvaro David
+//Villeda Patricio Arón Alejandro
+//Calderon Sabbagh Juan Alberto
+//Olicon Alvarez Jessica Guadalupe
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
