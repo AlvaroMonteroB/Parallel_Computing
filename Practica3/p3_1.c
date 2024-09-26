3bfeaafb367b7f09596d0537faa34bf10a5ef2b2
AlvaroMonteroB <alvaromonterob@outlook.com>
2024-09-26 17:34:30 -0600

    write to file

--- Practica3/p3_1.c
+++ Practica3/p3_1.c
@@ -4,9 +4,49 @@
 //Olicon Alvarez Jessica Guadalupe
 
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<wait.h>
-#include<sys/types.h>
-#include<sys/shm.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+void write_to_file(FILE *f,FILE *result,int i);
+
+int main(){
+
+
+
+
+    return 0;
+}
+
+
+
+
+void write_to_file(FILE *f,FILE *result,int i){
+    
+
+            //printf("%i aaa   ",process[i]);
+            //printf("Hola desde el proceso %d\n",getppid());
+            int value;
+            int vector[3];
+            for (int k = 0; k < 3; k++) {//rows
+                for (int j = 0; j < 9; j++) {//cols
+                    fscanf(f, "%i", &value);
+                        if (j == i) {
+                            vector[k]=value;
+                                    }
+                    }
+                }
+            int result_value=1;
+            for (int i=0;i<3;i++){
+                result_value*=vector[i];
+            }
+
+            fprintf(result,"%i ",result_value);
+            fflush(result);
+            fclose(f);
+            sleep(.001);
+            
+}
