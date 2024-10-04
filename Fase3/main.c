c1e86d3dc3bf1502c34c32812e708ae1924d1f44
AlvaroMonteroB <alvaromonterob@outlook.com>
2024-10-04 17:47:51 -0600

    fase 3

---
+++ Fase3/main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<wait.h>
+#include<sys/types.h>
+#include<math.h>
+#define pi=3.14159265359
+a0=4-((10*pi*pi)/9)
+
+
+float calc_term(int n, float x);
+
+int main(){
+    int n_max;
+    
+
+    return 0;
+}
+
+
+float calc_term(int n, float x){
+    float *an=(float*)(malloc(1,sizeof(float)));
+    an = (-20*cos(n*pi))/3*sqrt(n,2);
+    float *result=(float*)(malloc(1,sizeof(float)));
+
+    result an*cos((2*n*x))
+
+    free an;
+    free result;
+}
